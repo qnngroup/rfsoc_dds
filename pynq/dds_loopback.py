218bbe2502883900114d3361937ba5c7f9861dbe
Reed Foster <reed.a.foster@gmail.com>
2023-04-12 17:34:23 -0400

    added bitstreams

--- pynq/dds_loopback.py
+++ pynq/dds_loopback.py
@@ -5,13 +5,14 @@ import xrfclk
 import numpy as np
 import matplotlib.pyplot as plt
 from axitimer import AxiTimerDriver
+from axitxfifo import AxiStreamFifoDriver
 import scipy.signal
 import scipy.io
 import time
 import serial
 
 class DDSOverlay(Overlay):
-    def __init__(self, bitfile_name=None, dbg=False, plot=False, n_buffers=1, **kwargs):
+    def __init__(self, bitfile_name=None, dbg=False, plot=False, n_buffers=1, phase_calibration=True, **kwargs):
         if bitfile_name is None:
             this_dir = os.path.dirname(__file__)
             bitfile_name = os.path.join(this_dir, 'hw', 'top.bit')
@@ -20,42 +21,28 @@ class DDSOverlay(Overlay):
         super().__init__(bitfile_name, **kwargs)
         self.dma_recv = self.axi_dma_0.recvchannel
         self.capture_trig = self.axi_gpio_capture.channel1[0]
-        self.pinc = self.axi_gpio_pinc.channel1
-        self.cos_scale = self.axi_gpio_scale.channel1
+        self.pinc = self.axi_fifo_pinc
+        self.cos_scale = self.axi_fifo_scale
         self.timer = self.axi_timer_0
+        self.lmh6401 = self.axi_fifo_lmh6401
         xrfclk.set_ref_clks(lmk_freq=122.88, lmx_freq=409.6)
         self.f_samp = 4.096e9 # Hz
         self.phase_bits = 24
         self.timer.start_tmr()
-        self.dma_frame_size = 32*65536
+        self.dma_frame_shape = (32*32768, 2)
+        self.dma_frame_size = self.dma_frame_shape[0]*self.dma_frame_shape[1]
         alloc_bytes = n_buffers * self.dma_frame_size * 2 
         if (alloc_bytes > 3e9):
             raise ValueError(f"refusing to allocate {round(alloc_bytes/(2**20))}MiB of DMA buffer, try again with smaller n_buffers")
-        self.dma_buffers = [allocate(shape=(self.dma_frame_size,), dtype=np.int16) for i in range(n_buffers)]
+        self.dma_buffers = [allocate(shape=self.dma_frame_shape, dtype=np.int16) for i in range(n_buffers)]
         self.dbg = dbg
         self.plot = plot
-        self.raspi = None
-        for i in range(10):
-            try:
-                self.raspi = serial.Serial(f'/dev/ttyACM{i}')
-                if self.dbg:
-                    print(f'connected to raspberry pi pico on {self.raspi.name}')
-                break
-            except (OSError, serial.SerialException):
-                pass
-        if self.raspi is None:
-            raise Warning('could not connect to raspberry pi pico. variable gain will not work')
-        else:
-            while self.raspi.in_waiting:
-                l = self.raspi.readline()
-                if self.dbg:
-                    print(l)
     
     def set_freq_hz(self, freq_hz):
         pinc = int((freq_hz/self.f_samp)*(2**self.phase_bits))
         if self.dbg:
             print(f'setting pinc to {pinc} ({freq_hz:.3e}Hz)')
-        self.pinc.write(pinc,0xffffff)
+        self.pinc.send_tx_pkt([pinc])
     
     def set_dac_atten_dB(self, atten_dB):
         scale = round(atten_dB/6)
@@ -63,24 +50,16 @@ class DDSOverlay(Overlay):
             raise ValueError("cannot set attenuation less than 0dB or more than 90dB")
         if self.dbg:
             print(f'setting cos_scale to {scale} ({6*scale}dB attenuation)')
-        self.cos_scale.write(scale, 0xf)
+        self.cos_scale.send_tx_pkt([scale])
     
     def set_vga_atten_dB(self, atten_dB):
         atten_dB = round(atten_dB)
+        if atten_dB < 0 or atten_dB > 32:
+            raise ValueError("atten_dB out of range, pick a number between 0 and 32dB")
+        packet = 0x0200 | atten_dB & 0x3f # address 0x02, 6-bit data atten_dB
         if self.dbg:
             print(f'setting vga attenuation to {atten_dB}dB')
-        send_bytes = b'\r\n'
-        if atten_dB == 0:
-            send_bytes = b'0' + send_bytes
-        else:
-            while atten_dB > 0:
-                send_bytes = ((atten_dB % 10) + ord('0')).to_bytes(1,byteorder='big') + send_bytes
-                atten_dB //= 10
-        self.raspi.write(send_bytes)
-        while self.raspi.in_waiting:
-            l = self.raspi.readline()
-            if self.dbg:
-                print(l)
+        self.lmh6401.send_tx_pkt([packet])
     
     def realloc_buffers(self, n_buffers):
         if len(self.dma_buffers) == n_buffers:
@@ -89,7 +68,7 @@ class DDSOverlay(Overlay):
         if (alloc_bytes > 1e9):
             raise ValueError(f"refusing to allocate {round(alloc_bytes/(2**20))}MiB of DMA buffer, try again with smaller n_buffers")
         del self.dma_buffers
-        self.dma_buffers = [allocate(shape=(self.dma_frame_size,), dtype=np.int16) for i in range(n_buffers)]
+        self.dma_buffers = [allocate(shape=self.dma_frame_shape, dtype=np.int16) for i in range(n_buffers)]
     
     def capture_data(self, buffer, N_samp=128, OSR=256):
         self.capture_trig.on()
@@ -107,8 +86,10 @@ class DDSOverlay(Overlay):
             tvec = np.linspace(0,N_samp/self.f_samp*1e9,N_samp,endpoint=False)
             tvec_osr = np.linspace(0,N_samp/self.f_samp*1e9,N_samp*OSR,endpoint=False)
             plt.figure()
-            plt.plot(tvec,buffer[N_samp:2*N_samp], '.')
-            plt.plot(tvec_osr,scipy.signal.resample_poly(np.array(buffer[:8*N_samp],dtype=np.float32),OSR,1)[N_samp*OSR:2*N_samp*OSR], '-')
+            plt.plot(tvec,buffer[N_samp:2*N_samp,0], '.')
+            plt.plot(tvec_osr,scipy.signal.resample_poly(np.array(buffer[:8*N_samp,0],dtype=np.float32),OSR,1)[N_samp*OSR:2*N_samp*OSR], '-')
+            plt.plot(tvec,buffer[N_samp:2*N_samp,1], '.')
+            plt.plot(tvec_osr,scipy.signal.resample_poly(np.array(buffer[:8*N_samp,1],dtype=np.float32),OSR,1)[N_samp*OSR:2*N_samp*OSR], '-')
     
     def do_freq_sweep(self, name, dac_atten_dB, vga_atten_dB, freqs):
         self.set_dac_atten_dB(dac_atten_dB)
@@ -117,4 +98,4 @@ class DDSOverlay(Overlay):
         for i,freq in enumerate(freqs):
             self.set_freq_hz(freq)
             self.capture_data(self.dma_buffers[i])
-        scipy.io.savemat(name, {"tdata": np.array(self.dma_buffers), "freqs_hz": np.array(freqs), "dma_size": self.dma_frame_size, "dac_atten_dB": dac_atten_dB})
+        scipy.io.savemat(name, {"tdata": np.array(self.dma_buffers), "freqs_hz": np.array(freqs), "dma_shape": self.dma_frame_shape, "dac_atten_dB": dac_atten_dB})
