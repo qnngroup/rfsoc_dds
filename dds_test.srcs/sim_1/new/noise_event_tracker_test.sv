f709da7059b00ea3a1932626c65123350bedc070
Reed Foster <reed.a.foster@gmail.com>
2023-09-29 14:43:22 -0400

    compressive sample buffer for analyzing noise signal is going well; TODO test decimation capability

--- dds_test.srcs/sim_1/new/noise_event_tracker_test.sv
+++ dds_test.srcs/sim_1/new/noise_event_tracker_test.sv
@@ -1,4 +1,234 @@
 `timescale 1ns / 1ps
 module noise_event_tracker_test();
 
+logic clk = 0;
+localparam CLK_RATE_HZ = 100_000_000;
+always #(0.5s/CLK_RATE_HZ) clk = ~clk;
+
+logic reset;
+logic start, stop, mode;
+logic [15:0] threshold_high, threshold_low;
+
+Axis_If #(.DWIDTH(16)) data_in_00_if();
+Axis_If #(.DWIDTH(16)) data_in_02_if();
+Axis_If #(.DWIDTH(128)) data_out_if();
+Axis_If #(.DWIDTH(35)) config_in_if();
+
+assign config_in_if.data = {mode, start, stop, threshold_high, threshold_low};
+
+noise_event_tracker #(
+  .BUFFER_DEPTH(32), // 32x128 = 128 samples from each channel can be stored
+  .SAMPLE_WIDTH(16),
+  .AXI_MM_WIDTH(128),
+  .DECIMATION_BELOW_THRESH(1) // don't decimate for now
+) dut_i (
+  .clk,
+  .reset,
+  .data_out(data_out_if),
+  .data_in_00(data_in_00_if),
+  .data_in_02(data_in_02_if),
+  .config_in(config_in_if)
+);
+
+logic [15:0] data_sent_00 [$];
+logic [15:0] data_sent_02 [$];
+
+int word_index;
+int words_stored;
+
+always @(posedge clk) begin
+  if (reset) begin
+    data_in_00_if.data <= '0;
+    data_in_02_if.data <= '0;
+    word_index = 0;
+    words_stored = 0;
+  end else begin
+    if (data_in_00_if.valid && data_in_00_if.ready) begin
+      data_sent_00.push_front({data_in_00_if.data[15:2], 1'b0, 1'b0});
+      data_in_00_if.data <= $urandom_range(0,1<<16);
+      word_index = word_index + 1;
+    end
+    if (data_in_02_if.valid && data_in_02_if.ready) begin
+      data_sent_02.push_front({data_in_02_if.data[15:2], 1'b1, 1'b0});
+      data_in_02_if.data <= $urandom_range(0,1<<16);
+      word_index = word_index + 1;
+    end
+    if (word_index >= 8) begin
+      word_index = word_index % 8;
+      words_stored = words_stored + 1;
+    end
+  end
+end
+
+int error_count_00;
+int error_count_02;
+int words_checked;
+logic [15:0] data_received_00 [$];
+logic [15:0] data_received_02 [$];
+// check output
+always @(posedge clk) begin
+  if (reset) begin
+    words_checked = 0;
+    error_count_00 = 0;
+    error_count_02 = 0;
+  end else begin
+    if (data_out_if.valid && data_out_if.ready) begin
+      words_checked = words_checked + 1;
+      for (int i = 0; i < 8; i++) begin
+        if (data_out_if.data[i*16+1]) begin
+          // channel 02
+          data_received_02.push_front(data_out_if.data[i*16+:16]);
+        end else begin
+          // channel 00
+          data_received_00.push_front(data_out_if.data[i*16+:16]);
+        end
+      end
+    end
+  end
+end
+
+initial begin
+  reset <= 1'b1;
+  start <= 1'b0;
+  stop <= 1'b0;
+  mode <= 1'b0;
+  config_in_if.valid <= 1'b0;
+  data_in_00_if.valid <= 1'b0;
+  data_in_02_if.valid <= 1'b0;
+  data_out_if.ready <= 1'b0;
+  repeat (500) @(posedge clk);
+  reset <= 1'b0;
+  repeat (100) @(posedge clk);
+
+  ///////////////////////////////////////
+  // first trial
+  ///////////////////////////////////////
+
+  start <= 1'b1;
+  threshold_high <= '0;
+  threshold_low <= '1;
+  config_in_if.valid <= 1'b1;
+  @(posedge clk);
+  start <= 1'b0;
+  config_in_if.valid <= 1'b0;
+  repeat (100) @(posedge clk);
+  // send some samples in
+  repeat (50) begin
+    // first, separate samples by a couple cycles
+    data_in_00_if.valid <= 1'b1; // only will ever get samples one at a time
+    @(posedge clk);
+    data_in_00_if.valid <= 1'b0;
+    repeat (2) @(posedge clk);
+    data_in_02_if.valid <= 1'b1;
+    @(posedge clk);
+    data_in_02_if.valid <= 1'b0;
+    repeat (20) @(posedge clk);
+  end
+  repeat (50) begin
+    // next, have samples arrive on the same cycle
+    repeat (100) @(posedge clk);
+    data_in_00_if.valid <= 1'b1;
+    data_in_02_if.valid <= 1'b1;
+    @(posedge clk);
+    data_in_00_if.valid <= 1'b0;
+    data_in_02_if.valid <= 1'b0;
+    repeat (20) @(posedge clk);
+  end
+  // stop capture and read out
+  stop <= 1'b1;
+  config_in_if.valid <= 1'b1;
+  @(posedge clk);
+  stop <= 1'b0;
+  config_in_if.valid <= 1'b0;
+  repeat (500) @(posedge clk);
+  // read out
+  data_out_if.ready <= 1'b1;
+  repeat (10) @(posedge clk);
+  data_out_if.ready <= 1'b0;
+  repeat (100) @(posedge clk);
+  data_out_if.ready <= 1'b1;
+  repeat (1000) @(posedge clk);
+
+  ///////////////////////////////////////
+  // second trial
+  ///////////////////////////////////////
+
+  start <= 1'b1;
+  threshold_high <= '0;
+  threshold_low <= '1;
+  config_in_if.valid <= 1'b1;
+  @(posedge clk);
+  start <= 1'b0;
+  config_in_if.valid <= 1'b0;
+  repeat (100) @(posedge clk);
+  // send some samples in
+  repeat (70) begin
+    // first, have samples arrive on the same cycle
+    repeat (100) @(posedge clk);
+    data_in_00_if.valid <= 1'b1;
+    data_in_02_if.valid <= 1'b1;
+    @(posedge clk);
+    data_in_00_if.valid <= 1'b0;
+    data_in_02_if.valid <= 1'b0;
+    repeat (20) @(posedge clk);
+  end
+  repeat (20) begin
+    // next, separate samples by a couple cycles
+    data_in_00_if.valid <= 1'b1; // only will ever get samples one at a time
+    @(posedge clk);
+    data_in_00_if.valid <= 1'b0;
+    repeat (2) @(posedge clk);
+    data_in_02_if.valid <= 1'b1;
+    @(posedge clk);
+    data_in_02_if.valid <= 1'b0;
+    repeat (20) @(posedge clk);
+  end
+  // stop capture and read out
+  stop <= 1'b1;
+  config_in_if.valid <= 1'b1;
+  @(posedge clk);
+  stop <= 1'b0;
+  config_in_if.valid <= 1'b0;
+  repeat (500) @(posedge clk);
+  // read out
+  data_out_if.ready <= 1'b1;
+  repeat (10) @(posedge clk);
+  data_out_if.ready <= 1'b0;
+  repeat (100) @(posedge clk);
+  data_out_if.ready <= 1'b1;
+  repeat (1000) @(posedge clk);
+  // check everything
+  if (words_stored != words_checked) begin
+    $warning("didn't receive same number of words as words sent (received: %d, sent: %d)",
+      words_stored, words_checked);
+  end
+  if (data_sent_00.size() != data_received_00.size()) begin
+    $warning("ch00 incorrect amount of samples received (received: %d, sent: %d)",
+      data_received_00.size(), data_sent_00.size());
+  end
+  if (data_sent_02.size() != data_received_02.size()) begin
+    $warning("ch02 incorrect amount of samples received (received: %d, sent: %d)",
+      data_received_02.size(), data_sent_02.size());
+  end
+  while (data_received_00.size() > 0 && data_sent_00.size() > 0) begin
+    if ((data_received_00[$] & 16'hfffc) != (data_sent_00[$] & 16'hfffc)) begin
+      $warning("ch00 mismatch (got %x, expected %x)", data_received_00[$] & 16'hfffc, data_sent_00[$] & 16'hfffc);
+      error_count_00 = error_count_00 + 1;
+    end
+    data_sent_00.pop_back();
+    data_received_00.pop_back();
+  end
+  while (data_received_02.size() > 0 && data_sent_02.size() > 0) begin
+    if ((data_received_02[$] & 16'hfffc) != (data_sent_02[$] & 16'hfffc)) begin
+      $warning("ch02 mismatch (got %x, expected %x)", data_received_02[$] & 16'hfffc, data_sent_02[$] & 16'hfffc);
+      error_count_02 = error_count_02 + 1;
+    end
+    data_sent_02.pop_back();
+    data_received_02.pop_back();
+  end
+  $info("error_count = (ch00: %d, ch02: %d)", error_count_00, error_count_02);
+  $finish;
+end
+
+
 endmodule
--- dds_test.srcs/sources_1/new/noise_event_tracker.sv
+++ dds_test.srcs/sources_1/new/noise_event_tracker.sv
@@ -11,7 +11,7 @@ module noise_event_tracker #(
   parameter int AXI_MM_WIDTH = 128,
   parameter int DECIMATION_BELOW_THRESH = 10000 // 200S/s
 ) (
-  input wire clk, reset
+  input wire clk, reset,
   Axis_If.Master_Full data_out, // packed collection of samples
   Axis_If.Slave_Simple data_in_00, // two least significant bits of each sample will be trimmed off
   Axis_If.Slave_Simple data_in_02,
@@ -39,7 +39,7 @@ always_ff @(posedge clk) begin
     threshold_low <= '1;
     threshold_high <= '0;
   end else begin
-    if (config_in.ready && config_in.valid) begin
+    if (config_in.valid) begin
       mode <= config_in_mode;
       threshold_high <= config_in.data[2*SAMPLE_WIDTH-1:SAMPLE_WIDTH];
       threshold_low <= config_in.data[SAMPLE_WIDTH-1:0];
@@ -69,10 +69,10 @@ assign data_in_valid[1] = data_in_02.valid;
 
 // datapath signals
 logic [SAMPLE_WIDTH-1:0] data_in_d [2];
-logic [1:0] data_valid_d;
+logic [1:0] data_in_valid_d [2];
 logic [$clog2(DECIMATION_BELOW_THRESH)-1:0] dec_counter [2];
-logic [SAMPLE_WIDTH-1:0] fifo_out [2];
-logic [1:0] fifo_read;
+logic [SAMPLE_WIDTH-1:0] fifo_out_data [2];
+logic [1:0] fifo_ready;
 logic [1:0] fifo_not_empty;
 
 // apply fifos to merge data from two axi streams
@@ -81,9 +81,9 @@ generate begin: fifo_gen
     Axis_If #(.DWIDTH(SAMPLE_WIDTH)) fifo_in ();
     Axis_If #(.DWIDTH(SAMPLE_WIDTH)) fifo_out ();
 
-    assign fifo_in.valid = dec_counter[i] == 0;
+    assign fifo_in.valid = ((dec_counter[i] == 0) || mode == 0) && data_in_valid_d[i][1];
     assign fifo_not_empty[i] = fifo_out.valid;
-    assign fifo_out[i] = fifo_out.data;
+    assign fifo_out_data[i] = fifo_out.data;
     assign fifo_out.ready = fifo_ready[i];
 
     // ignore fifo_in.ready
@@ -93,8 +93,8 @@ generate begin: fifo_gen
 
     always_ff @(posedge clk) begin
       data_in_d[i] <= data_in[i];
-      data_in_valid_d[i] <= data_in_valid[i];
-      fifo_in.data <= {data_in_d[i][SAMPLE_WIDTH-1:2], i, noise_level[i]};
+      data_in_valid_d[i] <= {data_in_valid_d[i][0], data_in_valid[i]};
+      fifo_in.data <= {data_in_d[i][SAMPLE_WIDTH-1:2], 1'(i), noise_level[i]};
       if (reset) begin
         noise_level[i] <= '0;
         dec_counter[i] <= '0;
@@ -103,17 +103,16 @@ generate begin: fifo_gen
         if (data_in_valid[i]) begin
           if (data_in[i] > threshold_high) begin
             noise_level[i] <= 1'b1;
-          end
-          if (data_in[i] < threshold_low) begin
+          end else if (data_in[i] < threshold_low) begin
             noise_level[i] <= 1'b0;
           end
         end
         // manage decimation counter
         if (noise_level[i]) begin
-          dec_counter <= '0;
+          dec_counter[i] <= '0;
         end else begin
           // noise is low, so update decimation counter
-          if (data_in_valid_d[i]) begin
+          if (data_in_valid_d[i][0]) begin
             if (dec_counter[i] == DECIMATION_BELOW_THRESH - 1) begin
               dec_counter[i] <= '0;
             end else begin
@@ -134,6 +133,7 @@ generate begin: fifo_gen
       .data_in(fifo_in)
     );
   end
+end
 endgenerate
 
 //////////////////////////////////////////////////////
@@ -144,15 +144,15 @@ logic fifo_select;
 always_comb begin
   if (fifo_not_empty[0]) begin
     fifo_select = 0;
-    fifo_read = 2'b01;
+    fifo_ready = 2'b01;
   end else begin
     if (fifo_not_empty[1]) begin
       fifo_select = 1;
-      fifo_read = 2'b10;
+      fifo_ready = 2'b10;
     end else begin
       // default to fifo 0
       fifo_select = 0;
-      fifo_read = 2'b0;
+      fifo_ready = 2'b0;
     end
   end
 end
@@ -162,31 +162,38 @@ end
 localparam int WORD_SIZE = 2**($clog2(SAMPLE_WIDTH));
 localparam int WORD_SELECT_BITS = $clog2(AXI_MM_WIDTH) - $clog2(SAMPLE_WIDTH);
 logic [WORD_SELECT_BITS-1:0] word_select;
-logic [AXI_MM_WIDTH-1:0] data_in_word, data_out_word;
+logic [AXI_MM_WIDTH-1:0] data_in_word;
+logic data_in_word_valid;
 always_ff @(posedge clk) begin
   if (reset) begin
     word_select <= '0;
     data_in_word <= '0;
+    data_in_word_valid <= 1'b0;
   end else begin
     if (fifo_not_empty[fifo_select]) begin
       // word will continuously update and only be read into sample buffer
       // when a capture is started. yes this is a little inefficient but
       // I didn't think of it in advance and I don't want to rewrite the input
       // logic
-      data_in_word[word_select*WORD_SIZE+:WORD_SIZE] <= fifo_out[fifo_select];
+      data_in_word[word_select*WORD_SIZE+:WORD_SIZE] <= fifo_out_data[fifo_select];
       word_select <= word_select + 1'b1; // rolls over since AXI_MM_WIDTH is a power of 2
     end
+    data_in_word_valid <= fifo_not_empty[fifo_select] && (word_select == 2**WORD_SELECT_BITS - 1);
   end
 end
 
 //////////////////////////////////////////////////////
 // main state machine and sample buffer
 //////////////////////////////////////////////////////
-enum {IDLE, CAPTURE, TRANSFER} state;
+enum {IDLE, CAPTURE, POSTCAPTURE, PRETRANSFER, TRANSFER} state;
 logic [AXI_MM_WIDTH-1:0] buffer [BUFFER_DEPTH];
-logic [$clog2(BUFFER_DEPTH)-1:0] write_addr, read_addr;
-logic data_out_valid; // extra valid and last to match latency of BRAM
-logic data_out_last;
+logic [$clog2(BUFFER_DEPTH)-1:0] write_addr, read_addr, read_stop_addr;
+logic [AXI_MM_WIDTH-1:0] data_out_word [2];
+logic [2:0] data_out_valid; // extra valid and last to match latency of BRAM
+logic [1:0] data_out_last;
+assign data_out.data = data_out_word[1];
+assign data_out.valid = data_out_valid[2];
+assign data_out.last = data_out_last[1];
 // state machine
 always_ff @(posedge clk) begin
   if (reset) begin
@@ -199,27 +206,71 @@ always_ff @(posedge clk) begin
       CAPTURE: begin
         if ((config_in.valid && config_in_stop)
             || (write_addr == BUFFER_DEPTH - 1)) begin
-          state <= TRANSFER;
+          state <= POSTCAPTURE;
         end
       end
-      TRANSFER: if (data_out_last) state <= IDLE;
+      POSTCAPTURE: state <= PRETRANSFER;
+      PRETRANSFER: if (data_out_valid[1]) state <= TRANSFER;
+      TRANSFER: if (data_out.last && data_out.valid && data_out.ready) state <= IDLE;
     endcase
   end
 end
-// buffer logic
+// sample buffer logic
 // let's actually make the data_out_valid to spec
 // will be a little bit of work, since we want to register the output (so
 // valid will have to be delayed appropriately), but valid shouldn't
 // depend on ready signal from DMA IP (according to spec).
 always_ff @(posedge clk) begin
   if (reset) begin
+    data_out_valid <= '0;
+    data_out_last <= '0;
+    read_addr <= '0;
+    write_addr <= '0;
   end else begin
     unique case (state)
       IDLE: begin
+        data_out_valid <= '0;
+        data_out_last <= '0;
+        read_addr <= '0;
+        write_addr <= '0;
       end
       CAPTURE: begin
+        if (data_in_word_valid) begin
+          buffer[write_addr] <= data_in_word;
+          write_addr <= write_addr + 1'b1;
+          read_stop_addr <= write_addr;
+        end
+      end
+      POSTCAPTURE: begin
+        data_out_valid <= {data_out_valid[1:0], 1'b1};
+        data_out_word[0] <= buffer[read_addr];
+        data_out_word[1] <= data_out_word[0];
+      end
+      PRETRANSFER: begin
+        // repeats until data_out.valid is high
+        data_out_valid <= {data_out_valid[1:0], 1'b0};
+        data_out_word[0] <= buffer[read_addr];
+        data_out_word[1] <= data_out_word[0];
       end
       TRANSFER: begin
+        if (data_out.ready) begin // as long as output is ready, increment address
+          data_out_word[0] <= buffer[read_addr];
+          data_out_word[1] <= data_out_word[0];
+          if (read_addr == read_stop_addr) begin
+            // stop reading after we've finished reading everything that's
+            // been written to the buffer
+            // set last signal high and valid signal low
+            data_out_valid <= {data_out_valid[1:0], 1'b0};
+            read_addr <= read_stop_addr;
+            data_out_last <= {data_out_last[0], ~data_out_last[0]};
+          end else begin
+            // while we're not done reading from the buffer,
+            // increment the read address and set valid signal high
+            read_addr <= read_addr + 1'b1;
+            data_out_valid <= {data_out_valid[1:0], 1'b1};
+            data_out_last <= {data_out_last[0], 1'b0};
+          end
+        end
       end
     endcase
   end
